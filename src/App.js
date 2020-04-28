27fb4d79ebc85ad55d2135d5025f41fe439b52cc
Sai Dhruv <sai_dhruv@hotmail.com>
2020-04-28 21:24:29 +0530

    React Update

---
+++ src/App.js
@@ -0,0 +1,19 @@
+/** @format */
+
+import React from 'react';
+import logo from './assets/images/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>This is Sai Dhruv's Website.</p>
+        <p>It is being upgraded. Kindly come back later.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
