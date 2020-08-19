c473858686fcbc110c28f54adc6d3be092b990ad
Sai Dhruv <sai@spraoi.ai>
2020-08-19 12:38:25 +0530

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## Welcome to the source of sdkv.in
-
-It is still under development.
-
---
+++ precache-manifest.f8262f370335f1d6694de8efaa2d468b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47054f7c2ec95e4b0b292739228df217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6297bb499239978eb381",
+    "url": "/static/css/main.7f657d0d.chunk.css"
+  },
+  {
+    "revision": "70deb242ec90c8edd20e",
+    "url": "/static/js/2.879bde96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.879bde96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6297bb499239978eb381",
+    "url": "/static/js/main.76ce9128.chunk.js"
+  },
+  {
+    "revision": "a26861783d3bc8545216",
+    "url": "/static/js/runtime-main.bcb715d7.js"
+  },
+  {
+    "revision": "1222269a46d4b5ed0cb8b6d9fc57e250",
+    "url": "/static/media/Sai.1222269a.pdf"
+  },
+  {
+    "revision": "e49d7ae14aaca72fbd15120ed19d1430",
+    "url": "/static/media/ZillaSlab.e49d7ae1.ttf"
+  }
+]);
